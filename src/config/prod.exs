25f71961cbce261d01017ab7be1cca8873b31609
shibani <shibani@8thlight.com>
2018-08-16 16:03:39 -0400

    adding gigalixir

--- src/config/prod.exs
+++ src/config/prod.exs
@@ -61,4 +61,19 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+
+config :yelp, YelpWeb.Endpoint,
+  load_from_system_env: true,
+  # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
+  server: true, # Without this line, your app will not start the web server!
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :yelp, Yelp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}",
+  database: "",
+  ssl: false,
+  pool_size: 1 # Free tier db only allows 1 connection

30ad5a352b91e90eb4f71ff45ad9d801efade54b
shibani <shibani@8thlight.com>
2018-08-18 12:53:21 -0400

    removing distillery

--- src/mix.exs
+++ src/mix.exs
@@ -40,7 +40,6 @@ defmodule Yelp.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:distillery, "~> 1.0.0"},
       {:cowboy, "~> 1.0"}
     ]
   end
